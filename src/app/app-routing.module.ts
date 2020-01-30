ae504c97f84a37bdcf29d1eab16b0b5f70495f0e
Lee Douglas <work2give@gmail.com>
2020-01-30 17:11:33 -0500

    integrated toolbar from Ng Material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,11 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [{
   path: '',
   pathMatch: 'full',
+  redirectTo: 'home',
+},
+{
+  path: 'home',
+  pathMatch: 'full',
   component: HomeComponent,
 },
 {
